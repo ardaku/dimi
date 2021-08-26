17742c2c7d7494499758f4be893316ba6c9c341c
Jeron Aldaron Lau <aldaronlau@gmail.com>
2021-08-26 17:03:28 -0500

    Rename crate to Dimi

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+// Copyright © 2021 The Dimi Crate Developers
+//
+// Licensed under any of:
+// - Apache License, Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
+// - MIT License (https://mit-license.org/)
+// - Boost Software License, Version 1.0 (https://www.boost.org/LICENSE_1_0.txt)
+// At your option (See accompanying files LICENSE_APACHE_2_0.txt,
+// LICENSE_MIT.txt and LICENSE_BOOST_1_0.txt).  This file may not be copied,
+// modified, or distributed except according to those terms.
+//
+//! Asynchronous library for interfacing with MIDI devices.
+
+pub mod midi;
+
 #[cfg(test)]
 mod tests {
     #[test]
