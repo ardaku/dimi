68a741f477607370855825665c6589563ded82c5
Jeron Aldaron Lau <aldaronlau@gmail.com>
2021-08-27 03:11:59 -0500

    Add pasts dev-dependency

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,13 +1,40 @@
 // use std::io::{Result, Read};
 use std::fs::File;
 use std::io::{BufReader}; //, BufRead};
+use std::task::Poll::{self, Pending};
 
 use dimi::midi::{Event};
+use dimi::{Connector, Instrument};
+
+use pasts::Loop;
 
 //
 
+struct App {
+    // MIDI instrument connector
+    connector: Connector,
+}
+
+impl App {
+    fn connect(&mut self, instrument: Instrument) -> Poll<()> {
+        Pending
+    }
+
+    async fn run() {
+        let mut app = App {
+            connector: Connector::new(),
+        };
+
+        Loop::new(&mut app)
+            .when(|a| &mut a.connector, Self::connect)
+            .await
+    }
+}
+
 fn main() {
-    // Open file in read-only mode.
+    pasts::block_on(App::run())
+
+    /*// Open file in read-only mode.
     let file = File::open("/dev/snd/midiC1D0").expect("No MIDI");
     // Buffer the reader.
     let mut reader = BufReader::new(file);
@@ -18,5 +45,5 @@ fn main() {
     loop {
         // let midi = Midi::new(&mut reader);
         // dbg!(midi.map(Event::from));
-    }
+    }*/
 }
